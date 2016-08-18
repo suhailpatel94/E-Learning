774cd326c1ce29e06d34a950b46f45d86c0099c8
Mohammed Suhail Patel <suhailpatel94@gmail.com>
2016-08-18 08:04:50 +0530

    Update auth.js

--- app/config/auth.js
+++ app/config/auth.js
@@ -4,7 +4,7 @@ module.exports={
   
 'googleAuth' :{
   'clientID':'62552009635-fc43fk4ji8khgks82r77voj4fg9kflni.apps.googleusercontent.com',
-  'clientSecret':'Qo-GPH7s8H4BkjKaXvzh78e1',
+  'clientSecret':'your-key',
   'callbackURL':'http://localhost:3000/auth/google/callback'  
 }
   
